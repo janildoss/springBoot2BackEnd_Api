fdaec3ecf336cf8d2caa6542a98ea22bf21c2af5
janildoss <prjPedido_BackEnd_Spring>
2023-10-09 11:35:18 -0300

    Banco de dados H2 e criação automatica da base de dados

--- src/main/java/com/Api/springBoot2BackEnd_Api/domain/Categoria.java
+++ src/main/java/com/Api/springBoot2BackEnd_Api/domain/Categoria.java
@@ -1,13 +1,19 @@
 package com.Api.springBoot2BackEnd_Api.domain;
 
-import java.io.Serial;
 import java.io.Serializable;
 import java.util.Objects;
 
-public class Categoria implements Serializable {
-    @Serial
-    private static final long serialVersionUID = 1L;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+@Entity
+public class Categoria implements Serializable {
+    private static final long serialVersionUID = 1L;
+    
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
     private Integer id;
     private String nome;
 
