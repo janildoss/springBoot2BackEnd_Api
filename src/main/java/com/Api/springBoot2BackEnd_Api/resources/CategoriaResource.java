fa2e9d357a19e9c260af6fe81f56b48024fb6171
janildoss <prjPedido_BackEnd_Spring>
2023-10-09 12:36:02 -0300

    Testando a primeira classe de dominio-Categoria

--- src/main/java/com/Api/springBoot2BackEnd_Api/resources/CategoriaResource.java
+++ src/main/java/com/Api/springBoot2BackEnd_Api/resources/CategoriaResource.java
@@ -1,24 +1,25 @@
 package com.Api.springBoot2BackEnd_Api.resources;
 
-import com.Api.springBoot2BackEnd_Api.domain.Categoria;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
-import java.util.List;
+import com.Api.springBoot2BackEnd_Api.domain.Categoria;
+import com.Api.springBoot2BackEnd_Api.services.CategoriaService;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
-    @RequestMapping(method= RequestMethod.GET)
-    public List<Categoria>  listar(){
-        Categoria cat1 = new Categoria (1, "Informatica");
-        Categoria cat2 = new Categoria (2, "Escritorio");
-        List<Categoria> lista = new ArrayList<>();
-        lista.add(cat1);
-        lista.add(cat2);
-        return lista;
+	@Autowired
+	private CategoriaService service;
+    //@RequestMapping(method= RequestMethod.GET)
+    @GetMapping(value="/{id}")
+    public ResponseEntity<?> listar(@PathVariable Integer id){
+    	Categoria obj =  service.buscar(id);
+    	 return ResponseEntity.ok(obj);  
     }
 
 }
---
+++ src/main/java/com/Api/springBoot2BackEnd_Api/services/CategoriaService.java
@@ -0,0 +1,27 @@
+package com.Api.springBoot2BackEnd_Api.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.Api.springBoot2BackEnd_Api.domain.Categoria;
+import com.Api.springBoot2BackEnd_Api.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+	@Autowired
+	private CategoriaRepository repo;
+	
+	public Categoria buscar(Integer id) {
+		
+		Optional<Categoria> obj = repo.findById(id);		
+		return obj.orElseThrow(null) ;
+	}
+	
+	//Tratamento para quando pesquisar categoria nao existente POR ID
+	//	public Categoria find(Integer id) {
+	//		Optional<Categoria> obj = repo.findById(id);
+	//		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
+	//	}
+}
