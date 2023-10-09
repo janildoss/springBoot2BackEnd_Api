c65025b025b3599f0c819dbad08aacd09576b5f6
janildoss <prjPedido_BackEnd_Spring>
2023-10-08 22:48:38 -0300

    Projeto criado

---
+++ src/main/java/com/Api/springBoot2BackEnd_Api/SpringBoot2BackEndApiApplication.java
@@ -0,0 +1,14 @@
+package com.Api.springBoot2BackEnd_Api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot2BackEndApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoot2BackEndApiApplication.class, args);
+		System.out.println("Hello word");
+	}
+
+}
---
+++ src/test/java/com/Api/springBoot2BackEnd_Api/SpringBoot2BackEndApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Api.springBoot2BackEnd_Api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoot2BackEndApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
