6b0c7cc54e074c5f062e2f5bcea4911ffeedade1
janildoss <prjPedido_BackEnd_Spring>
2023-10-15 18:41:03 -0300

    Ajustes no endpoint /categorias/{id}

---
+++ src/main/java/com/Api/springBoot2BackEnd_Api/resources/exception/ResourceExceptionHandler.java
@@ -0,0 +1,23 @@
+package com.Api.springBoot2BackEnd_Api.resources.exception;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.Api.springBoot2BackEnd_Api.services.exceptions.ObjectNotFoundException;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+	
+	@ExceptionHandler(ObjectNotFoundException.class)
+	public ResponseEntity<StandardError> objectNotFound(ObjectNotFoundException e, HttpServletRequest request) {
+		StandardError err = new StandardError(HttpStatus.NOT_FOUND.value(),e.getMessage(),System.currentTimeMillis());
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
+	}
+	
+	
+	
+}
--- src/main/java/com/Api/springBoot2BackEnd_Api/services/CategoriaService.java
+++ src/main/java/com/Api/springBoot2BackEnd_Api/services/CategoriaService.java
@@ -7,17 +7,23 @@ import org.springframework.stereotype.Service;
 
 import com.Api.springBoot2BackEnd_Api.domain.Categoria;
 import com.Api.springBoot2BackEnd_Api.repositories.CategoriaRepository;
+import com.Api.springBoot2BackEnd_Api.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
 	@Autowired
 	private CategoriaRepository repo;
 	
-	public Categoria buscar(Integer id) {
+	/*public Categoria buscar(Integer id) {
 		
 		Optional<Categoria> obj = repo.findById(id);		
 		return obj.orElseThrow(null) ;
-	}
+	}*/
+	//Tratamento para quando pesquisar categoria nao existente POR ID
+		public Categoria buscar(Integer id) {
+			Optional<Categoria> obj = repo.findById(id);
+			return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
+		}
 	
 	//Tratamento para quando pesquisar categoria nao existente POR ID
 	//	public Categoria find(Integer id) {
