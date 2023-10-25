2ab4a6c295afdace1fcb1a44863a7caf44e72795
janildoss <prjPedido_BackEnd_Spring>
2023-10-25 15:28:26 -0300

    Correcoes:update em categoriaservice e construtor de pagamento

--- src/main/java/com/Api/springBoot2BackEnd_Api/services/CategoriaService.java
+++ src/main/java/com/Api/springBoot2BackEnd_Api/services/CategoriaService.java
@@ -38,10 +38,11 @@ public class CategoriaService {
 		}
 		
 		//No update se o id for null faz um insert
-	    public Categoria update(Categoria obj) {
-			find(obj.getId());
-			return repo.save(obj);
-		}
+		 public Categoria update(Categoria obj) {
+			    Categoria newObj = find(obj.getId());
+				updateData(newObj, obj);
+				return repo.save(newObj);
+			}
 		
 		 public void delete(Integer id) {
 				find(id);
@@ -65,5 +66,10 @@ public class CategoriaService {
 			//Metodo auxiliar que instancia um Categoria Dto
 		public Categoria fromDTO(CategoriaDTO objDto) {
 				return new Categoria(objDto.getId(),objDto.getNome());
+		}
+		
+		private void updateData(Categoria newObj,Categoria obj) {			
+			newObj.setNome(obj.getNome());
+			//newObj.setEmail(obj.getEmail());
 		}
 }
